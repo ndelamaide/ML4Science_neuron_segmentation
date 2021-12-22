a7126ce00091aa3674799a83d9ab739b0fae96b6
ndelamaide <nicolas.delamaide@gmail.com>
2021-12-22 21:55:54 +0100

    added metrics unet16_a

--- ternausnet/evaluation.py
+++ ternausnet/evaluation.py
@@ -112,4 +112,16 @@ def eval(from_file_names, to_path, num_classes):
 
                 jaccard.append(jaccard_)
 
+    metrics_array = np.array(jaccard)
+    mean = metrics_array.mean(axis=0)
+    std = metrics_array.std(axis=0)
+    max_ = metrics_array.max(axis=0)
+
+    
+    if num_classes > 1:
+        print("Jaccard neurons | mean : {mean}, std : {std}, max: {max}".format(mean=mean[0], std=std[0], max=max_[0]))
+        print("Jaccard axons | mean : {mean}, std : {std}, max: {max}".format(mean=mean[1], std=std[1], max=max_[1]))
+    else:
+        print("Jaccard neurons | mean : {mean}, std : {std}, max: {max}".format(mean=mean, std=std, max=max_))
+
     return jaccard
